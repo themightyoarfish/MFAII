2a598bad011d335534e0bd97ad9b9380b6ca9c71
Justin Shenk <shenkjustin@gmail.com>
2018-11-14 14:59:49 +0100

    Format according to PEP8 guidelines

--- Blatt 5/aufg17.py
+++ Blatt 5/aufg17.py
@@ -1,29 +1,29 @@
-import numpy as np 
+import numpy as np
 from numpy.linalg import inv, solve
 from scipy.linalg import svd
 
+
 def conditional_number(matrix):
-    _, sv, _     = svd(matrix)
-    sv_max       = sv[0]
+    _, sv, _ = svd(matrix)
+    sv_max = sv[0]
 
     _, sv_inv, _ = svd(inv(matrix))
-    sv_inv_max   = sv_inv[0]
+    sv_inv_max = sv_inv[0]
 
     return sv_max * sv_inv_max
 
+
 if __name__ == "__main__":
 
-    A_1 = np.array([[3, 1.001],
-                    [6, 1.997]])
-    
-    A_2 = np.array([[3, 1],
-                    [0, 2]])
+    A_1 = np.array([[3, 1.001], [6, 1.997]])
+
+    A_2 = np.array([[3, 1], [0, 2]])
 
     # 17 (2)
     print("%20s%s%20s" % ("-" * 20, "Aufgabe 17 (2)".center(20), "-" * 20))
     print("Condition number of A_1 is %f" % conditional_number(A_1))
     print("Condition number of A_2 is %f" % conditional_number(A_2))
-    
+
     print("%20s%s%20s" % ("-" * 20, "Aufgabe 17 (3)".center(20), "-" * 20))
 
     # 17 (3)
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     for A_i in [A_1, A_2]:
         for b in [b_1, b_2]:
             print("Solving Ax = b with A=\n%s\n and b=%s" % (np.array_str(A_i),
-                np.array_str(b)))
+                                                             np.array_str(b)))
             res = solve(A_i, b)
             print("Result = %s\n" % np.array_str(res))
--- Blatt 5/aufg20.py
+++ Blatt 5/aufg20.py
@@ -1,23 +1,26 @@
-import numpy as np 
+import numpy as np
 from math import sqrt
+
 
 def dot_prod(m1, m2):
     r = np.trace(m1.T.dot(m2))
     return r
 
+
 def norm(m):
-    r = sqrt(dot_prod(m,m))
-    return r 
+    r = sqrt(dot_prod(m, m))
+    return r
+
 
 if __name__ == "__main__":
-    
+
     # # part (1)
     # v_1 = np.array([[1,2], [1,0]])
     # v_2 = np.array([[-1,0], [1,0]])
     # v_3 = np.array([[-2,1], [0,1]])
 
     # w_1 = v_1 / norm(v_1)
-    # w_2_tmp = v_2 - dot_prod(v_2,w_1) * w_1 
+    # w_2_tmp = v_2 - dot_prod(v_2,w_1) * w_1
     # w_2 = w_2_tmp / norm(w_2_tmp)
     # w_3_tmp = v_3 - dot_prod(v_3,w_1) * w_1 - dot_prod(v_3, w_2) * w_2
     # w_3 = w_3_tmp / norm(w_3_tmp)
@@ -27,17 +30,18 @@ if __name__ == "__main__":
     # print("w_3 = \n%s" % np.array_str(w_3))
 
     # part (2)
-    v_1 = np.array([[1,0], [0,1]])
-    v_2 = np.array([[0,1], [1,0]])
-    v_3 = np.array([[1,1], [0,0]])
-    v_4 = np.array([[0,0], [1,1]])
+    v_1 = np.array([[1, 0], [0, 1]])
+    v_2 = np.array([[0, 1], [1, 0]])
+    v_3 = np.array([[1, 1], [0, 0]])
+    v_4 = np.array([[0, 0], [1, 1]])
 
     w_1 = v_1 / norm(v_1)
-    w_2_tmp = v_2 - dot_prod(v_2,w_1) * w_1 
+    w_2_tmp = v_2 - dot_prod(v_2, w_1) * w_1
     w_2 = w_2_tmp / norm(w_2_tmp)
-    w_3_tmp = v_3 - dot_prod(v_3,w_1) * w_1 - dot_prod(v_3, w_2) * w_2
+    w_3_tmp = v_3 - dot_prod(v_3, w_1) * w_1 - dot_prod(v_3, w_2) * w_2
     w_3 = w_3_tmp / norm(w_3_tmp)
-    w_4_tmp = v_4 - dot_prod(v_4,w_1) * w_1 - dot_prod(v_4, w_2) * w_2 - dot_prod(v_4, w_3) * w_3
+    w_4_tmp = v_4 - dot_prod(v_4, w_1) * w_1 - dot_prod(
+        v_4, w_2) * w_2 - dot_prod(v_4, w_3) * w_3
     w_4 = w_4_tmp / norm(w_4_tmp)
 
     print("w_1 = \n%s" % np.array_str(w_1))
